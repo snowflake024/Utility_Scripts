131ff75341f0bf5f9911a0d0360a8c0956b9f166
snowflake <59052038+snowflake024@users.noreply.github.com>
2022-11-01 10:35:11 +0000

    Update docker_debian_provision.sh

--- scripts/docker_debian_provision.sh
+++ scripts/docker_debian_provision.sh
@@ -1,1 +1,14 @@
-asd
+#!/usr/bin/env bash
+
+set +xe
+
+sudo -i -u root bash << EOF
+
+# Create a user to work with
+useradd -m -d /home/infra infra
+
+# Set arbirtrary password, recommended to change it later
+echo "arfni" | passwd infra
+
+# Set passwordless sudo for created user
+echo "infra ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
