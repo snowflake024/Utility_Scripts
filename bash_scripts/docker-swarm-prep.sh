f8bbf786348957d6c93dc3bacd1548024214fcb3
snowflake <59052038+snowflake024@users.noreply.github.com>
2022-11-01 12:18:35 +0000

    Update docker-swarm-prep.sh

--- bash_scripts/docker-swarm-prep.sh
+++ bash_scripts/docker-swarm-prep.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -x
 
-# Add hosts in /etc/hosts
-
+# Add node hosts in /etc/hosts
+echo "IP1" >> /etc/host
+echo "IP2" >> /etc/host
+echo "IP3" >> /etc/host
 
 # Firewall rules
+# TCP port 2377 for cluster management communications
+# TCP and UDP port 7946 for communication among nodes
+# UDP port 4789 for overlay network traffic
+ufw allow 2377/tcp
+ufw allow 7946/tcp
+ufw allow 7946/udp
+ufw allow 4789/udp
 
-    TCP port 2377 for cluster management communications.
-    TCP and UDP port 7946 for communication among nodes.
-    UDP port 4789 for overlay network traffic.
