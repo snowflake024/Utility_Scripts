616cafb02f5ce945e0e6952650d77932b8f1e1e5
snowflake <59052038+snowflake024@users.noreply.github.com>
2022-11-07 08:05:46 +0000

    Create rev_proxy_nginx_ubuntu2004.sh

---
+++ bash_scripts/rev_proxy_nginx_ubuntu2004.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -x
+
+# Installing Nginx
+apt install nginx -y
+
+# Enable in systemd
+systemctl enable --now nginx
+
+# Setup server block
+cat <<EOF >> /etc/nginx/sites-available/swarm_reverse_proxy.conf
+server {
+    listen 80;
+    listen [::]:80;
+
+    server_name dc2.waterviewtechnology.com;
+        
+    location / {
+        proxy_pass http://127.0.0.1:8081;
+        include proxy_params;
+        
+        # header filtering
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        
+    }
+}
+EOF
+
+# Symlink that shit
+ln -s /etc/nginx/sites-available/swarm_reverse_proxy.conf /etc/nginx/sites-enabled/
+
+# Restart NGINX
+nginx -t
+systemctl restart nginx   
