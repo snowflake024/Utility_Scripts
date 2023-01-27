b1d2b17c321a6136be31aa6ef58d0e5f37439799
snowflake <59052038+snowflake024@users.noreply.github.com>
2023-01-27 10:36:43 +0200

    Update crontab_access_check.py

--- checkmk_plugins/crontab_access_check/crontab_access_check.py
+++ checkmk_plugins/crontab_access_check/crontab_access_check.py
@@ -7,14 +7,20 @@ def discover_crontab_access_check(section):
     yield Service()
 
 
+def check_user(section):
+    for line in section:
+        if line.startswith('user', 8):
+            return line[13:]
+
+
+# Keep in mind that the bash script on agent side needs to be adapted for whichever user,
+# needs to be checked against access to its own crontab
 def check_crontab_access(section):
     for line in section:
-        if line[0].startswith("Failed-CRON"):
-            yield Result(state=State.CRIT, summary="Local user cannot access crontab")
+        if line[1].startswith("Failed-CRON"):
+            yield Result(state=State.CRIT, summary=f"Local user {check_user(section)} cannot access crontab")
             return
-    # Keep in mind that the bash script on agent side needs to be adapted for whichever user,
-    # needs to be checked against access to its own crontab
-    yield Result(state=State.OK, summary="Local user cannot access to crontab is OK")
+    yield Result(state=State.OK, summary=f"Local user {check_user(section)} cannot access to crontab is OK")
 
 
 register.check_plugin(
